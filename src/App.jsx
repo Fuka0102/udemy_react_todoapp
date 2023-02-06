e4d1c8b253a0459c474b3217da99bb0185453004
Fuka <mizunofuka0102@gmail.com>
2023-02-06 14:30:43 +0000

    jsxで構造を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力"></input>
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            ああああ
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            ああああ
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            ああああ
+            <button>戻す</button>
+          </li>
+          <li>
+            ああああ
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
