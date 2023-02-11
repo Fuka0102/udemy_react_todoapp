cb7028f0e3c1587ced45dea7863fa3d4ad5b82af
Fuka <mizunofuka0102@gmail.com>
2023-02-11 15:36:18 +0000

    登録できるtodoの上限を設定

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incomplementTodos.length >= 5}
       />
+      {incomplementTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodoは5個までです</p>
+      )}
       <IncomplementTodos
         incomplementTodos={incomplementTodos}
         onClickComplete={onClickComplete}
--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <div value={todoText} className="input-area" onChange={onChange}>
-      <input placeholder="TODOを入力"></input>
-      <button onClick={onClick}>追加</button>
+      <input disabled={disabled} placeholder="TODOを入力"></input>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
